790b910186f01a2c07b6064430594519e1d705dc
Shyam Patel <shyamp124@gmail.com>
2024-02-25 20:11:23 -0500

    Initial commit

---
+++ nvim/.config/nvim/nvim/lua/plugins/cyberdream.lua
@@ -0,0 +1,15 @@
+return {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    name = "cyberdream",
+    config = function()
+        require("cyberdream").setup({
+            -- Recommended - see "Configuring" below for more config options
+            transparent = true,
+            italic_comments = true,
+            hide_fillchars = true,
+            borderless_telescope = true,
+        })
+    end,
+}
---
+++ vim/.vimrc
@@ -0,0 +1,7 @@
+set number
+set relativenumber
+set scrolloff=10
+set shiftwidth=4
+set tabstop=4
+let &t_SI = "\e[6 q"
+let &t_EI = "\e[2 q"
