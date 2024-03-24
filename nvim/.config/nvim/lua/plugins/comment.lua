7a10632a928be5d5ff11535f2cae33a9cdbf0fc7
Shyam Patel <shyamp124@gmail.com>
2024-03-23 20:35:42 -0400

    Added nvim comment plugin

---
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,20 @@
+return
+{
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    },
+    lazy = false,
+    init = function()
+        require('Comment').setup()
+        vim.keymap.set("n", "<C-_>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
+        vim.keymap.set("x", "<C-_>", 
+        function() 
+            local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            require('Comment.api').toggle.linewise(vim.fn.visualmode())
+        end,
+        { noremap = true, silent = true })
+    end
+}
+
