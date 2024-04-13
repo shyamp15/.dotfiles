b90130fe46d4ebd225581844de6a21db97d3fe7a
Shyam Patel <shyam@mac.local>
2024-04-13 19:26:36 -0400

    Changed some keymappings

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -7,8 +7,8 @@ return
     lazy = false,
     init = function()
         require('Comment').setup()
-        vim.keymap.set("n", "<C-_>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
-        vim.keymap.set("x", "<C-_>", 
+        vim.keymap.set("n", "<C-l>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
+        vim.keymap.set("x", "<C-l>", 
         function() 
             local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
             vim.api.nvim_feedkeys(esc, 'nx', false)
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,9 +3,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     init = function() 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>s', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>g', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
     end
