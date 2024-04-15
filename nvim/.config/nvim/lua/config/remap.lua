7fbfda14824bd9c743c36776c6e8bbaff1ac5497
Shyam Patel <shyamp124@gmail.com>
2024-04-15 00:17:20 -0400

    Added ctrl + backspace remap and added more telescope remaps

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -18,3 +18,20 @@ vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Remap Ctrl + backspace (dependent on terminal) to delete word
+vim.keymap.set("i", "<C-H>", "<C-W>")
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
