17e7afba004573ca68e8e86fe1170f964ef5ed24
Shyam Patel <shyamp124@gmail.com>
2024-06-02 21:48:45 -0400

    Changed lsp configuration

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -1,27 +1,11 @@
 local plugins = {
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^4', -- Recommended
-        ft = { 'rust' },
-        init = function() 
-            --local on_attach = require("plugins.configs.lspconfig").on_attach
-            --local capabilities = require("plugins.configs.lspconfig").capabilities
-
-            vim.g.rustaceanvim = {
-                server = {
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                }
-            }
-        end
-    },
-    {
-        "rust-lang/rust.vim",
-        ft = { "rust" },
-        init =function()
-            vim.g.rustfmt_autosave = 1
-        end
-    }
+	{
+		"rust-lang/rust.vim",
+		ft = { "rust" },
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
 }
 
 return plugins
